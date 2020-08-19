714ee4386ab6c1ef37bd9f30bd3edfa6778c4a68
LocNguyen <quangloc.nguyen@mafc.com.vn>
2020-08-19 15:29:48 +0700

    display

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'David' },
+  { id: 13, name: 'Tommy' },
+  { id: 15, name: 'Mike' },
+  { id: 16, name: 'Sven' },
+  { id: 17, name: 'Jonny' },
+  { id: 18, name: 'Henry' },
+];
